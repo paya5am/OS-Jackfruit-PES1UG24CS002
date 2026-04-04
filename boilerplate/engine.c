a6757d57b2d0ad107e6a6e6b13d641fc5dc30728
Aakash Desai <PES1UG24CS006@pesuonline.onmicrosoft.com>
2026-04-04 12:46:18 +0530

    Day 2: multi-container runtime with metadata tracking

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -6,7 +6,16 @@
 #include <sys/mount.h>
 #include <sys/wait.h>
 #include <string.h>
+#define MAX_CONTAINERS 10
 
+struct container {
+    char id[32];
+    pid_t pid;
+    int running;
+};
+
+struct container containers[MAX_CONTAINERS];
+int container_count = 0;
 #define STACK_SIZE (1024 * 1024)
 static char child_stack[STACK_SIZE];
 
@@ -40,31 +49,49 @@ int child_func(void *arg) {
     perror("execl");
     return 1;
 }
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <rootfs>\n", argv[0]);
-        return 1;
-    }
-
-    char *rootfs = argv[1];
-
-    printf("[Parent] Launching container...\n");
-
+pid_t start_container(char *id, char *rootfs) {
     int flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD;
 
     pid_t pid = clone(child_func, child_stack + STACK_SIZE, flags, rootfs);
 
     if (pid < 0) {
         perror("clone");
-        return 1;
+        return -1;
     }
 
-    printf("[Parent] Container PID: %d\n", pid);
+    // store metadata
+    strcpy(containers[container_count].id, id);
+    containers[container_count].pid = pid;
+    containers[container_count].running = 1;
 
-    waitpid(pid, NULL, 0);
+    container_count++;
 
-    printf("[Parent] Container exited.\n");
+    printf("[Parent] Started container %s with PID %d\n", id, pid);
+
+    return pid;
+}
+int main() {
+    printf("[Parent] Starting multiple containers...\n");
+
+    start_container("alpha", "./rootfs-alpha");
+    start_container("beta", "./rootfs-beta");
+
+    // print metadata
+    printf("\nContainer List:\n");
+    printf("ID\tPID\tSTATE\n");
+
+    for (int i = 0; i < container_count; i++) {
+        printf("%s\t%d\t%s\n",
+            containers[i].id,
+            containers[i].pid,
+            containers[i].running ? "running" : "stopped");
+    }
+
+    // wait for containers
+    for (int i = 0; i < container_count; i++) {
+        waitpid(containers[i].pid, NULL, 0);
+        containers[i].running = 0;
+    }
 
     return 0;
 }
